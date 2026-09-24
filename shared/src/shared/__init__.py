7b31ee27d8049b7dffef9a83dd97bbc852835c2d
Viv <vivax3794@protonmail.com>
2026-09-24 09:50:11 +0200

    setup linter/testing/formatter

--- shared/src/shared/__init__.py
+++ shared/src/shared/__init__.py
@@ -1,2 +1,8 @@
-def add(a, b):
+"""Shared library between all 3 nodes for sharing protcol helpers.
+
+As well as sharing shared types and constants.
+"""
+
+def add(a: int, b: int) -> int:
+    """Add two numbers (just testing project setup)."""
     return a + b
